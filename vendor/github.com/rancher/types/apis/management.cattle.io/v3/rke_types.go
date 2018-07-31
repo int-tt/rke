18de2873c39096905f18551910ccb9810c49402e
int <kattu0426@gmail.com>
2018-08-01 08:07:10 +0900

    poc:add awscloudprovider setting

--- vendor/github.com/rancher/types/apis/management.cattle.io/v3/rke_types.go
+++ vendor/github.com/rancher/types/apis/management.cattle.io/v3/rke_types.go
@@ -555,6 +555,52 @@ type AzureCloudProvider struct {
 
 // AWSCloudProvider options
 type AWSCloudProvider struct {
+	// https://github.com/kubernetes/kubernetes/blob/master/pkg/cloudprovider/providers/aws/aws.go#L524-L568
+	// TODO: Is there any use for this?  We can get it from the instance metadata service
+	// Maybe if we're not running on AWS, e.g. bootstrap; for now it is not very useful
+	Zone string `yaml:"zone" json:"zone,omitempty" ini:"zone,omitempty"`
+
+	// The AWS VPC flag enables the possibility to run the master components
+	// on a different aws account, on a different cloud provider or on-premises.
+	// If the flag is set also the KubernetesClusterTag must be provided
+	VPC string `yaml:"vpc" json:"vpc,omitempty" ini:"vpc,omitempty"`
+	// SubnetID enables using a specific subnet to use for ELB's
+	SubnetID string `yaml:"subnet_id" json:"subnet_id,omitempty" ini:"subnet_id,omitempty"`
+	// RouteTableID enables using a specific RouteTable
+	RouteTableID string `yaml:"route_table_id" json:"route_table_id,omitempty" ini:"route_table_id,omitempty"`
+
+	// RoleARN is the IAM role to assume when interaction with AWS APIs.
+	RoleARN string `yaml:"role_arn" json:"role_arn,omitempty" ini:"role_arn,omitempty"`
+
+	// KubernetesClusterTag is the legacy cluster id we'll use to identify our cluster resources
+	KubernetesClusterTag string `yaml:"kubernetes_cluster_tag" json:"kubernetes_cluster_tag,omitempty" ini:"kubernetes_cluster_tag,omitempty"`
+	// KubernetesClusterID is the cluster id we'll use to identify our cluster resources
+	KubernetesClusterID string `yaml:"kubernetes_cluster_id" json:"kubernetes_cluster_id,omitempty" ini:"kubernetes_cluster_id,omitempty"`
+
+	//The aws provider creates an inbound rule per load balancer on the node security
+	//group. However, this can run into the AWS security group rule limit of 50 if
+	//many LoadBalancers are created.
+	//
+	//This flag disables the automatic ingress creation. It requires that the user
+	//has setup a rule that allows inbound traffic on kubelet ports from the
+	//local VPC subnet (so load balancers can access it). E.g. 10.82.0.0/16 30000-32000.
+	DisableSecurityGroupIngress string `yaml:"disable_security_group_ingress" json:"disable_security_group_ingress,omitempty" ini:"disable_security_group_ingress,omitempty"`
+
+	//AWS has a hard limit of 500 security groups. For large clusters creating a security group for each ELB
+	//can cause the max number of security groups to be reached. If this is set instead of creating a new
+	//Security group for each ELB this security group will be used instead.
+	ElbSecurityGroup string `yaml:"elb_security_group" json:"elb_security_group,omitempty" ini:"elb_security_group,omitempty"`
+
+	//During the instantiation of an new AWS cloud provider, the detected region
+	//is validated against a known set of regions.
+	//
+	//In a non-standard, AWS like environment (e.g. Eucalyptus), this check may
+	//be undesirable.  Setting this to true will disable the check and provide
+	//a warning that the check was skipped.  Please note that this is an
+	//experimental feature and work-in-progress for the moment.  If you find
+	//yourself in an non-AWS cloud and open an issue, please indicate that in the
+	//issue body.
+	DisableStrictZoneCheck bool `yaml:"disable_strict_zone_check" json:"disable_strict_zone_check,omitempty" ini:"disable_strict_zone_check,omitempty"`
 }
 
 type MonitoringConfig struct {
